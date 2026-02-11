b9287ecc5f94de8fdfde0eb9475fdd0f45340d1d
John Marc R. Sanchez <johnmarc.sanchez@neu.edu.ph>
2026-02-11 05:57:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9437026762-d0198",
+  "appId": "1:208493122330:web:5ac5a9574119def938d2a3",
+  "apiKey": "AIzaSyDnO9Cg6Qvu_Iv6ihSYwg6AwkmYo9ZJQ64",
+  "authDomain": "studio-9437026762-d0198.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "208493122330"
+};
